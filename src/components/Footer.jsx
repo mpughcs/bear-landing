ae1b1f57e81cd5c063da609c318c5e1b878d7742
Max Pugh <mpugh@chapman.edu>
2024-02-21 13:36:44 -0800

    migrated appropriate nav and footer code from main bc i forgot to commit them before starting branch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,18 @@ import MailChimpSub from "./MailChimpSub";
 export default function Footer() {
     return (
 
-        <footer className="footer p-10 bg-base-200 text-base-content">
-        
-            <nav>
+        <footer className="footer p-4 md:p-10 bg-base-200 text-base-content justify-start gap-10">
+
+            <nav className="">
                 <h6 className="footer-title">Company</h6>
+
                 <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Press kit</a>
+                <a className="link link-hover" onClick={() => document.getElementById('my_modal_5').showModal()} >Contact</a>
+
             </nav>
+
             <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-            </nav>
-            <nav>
-            <MailChimpSub />
+                <MailChimpSub />
             </nav>
         </footer>
 
