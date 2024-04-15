45ef49fe63d2f371d94b9e52c9038ab8e9f2371a
Max Pugh <mpugh@chapman.edu>
2024-04-15 14:59:54 -0700

    fixed the header and footer not showing on privacy policy

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function PrivacyPolicy() {
   return (
-    <iframe src='/privacy-policy.html' title='Privacy Policy' className='w-full h-full' frameBorder='0' style={{ height: '100vh' }} scrolling='auto'></iframe>
+        <div style={{ paddingTop: '20px', paddingBottom: '50px' }}> {/* Adjust padding as per the actual height of NavBar and Footer */}
+        <iframe src='/privacy-policy.html' title='Privacy Policy' className='w-full h-auto' frameBorder='0' style={{ height: 'calc(100vh - 100px)' }} scrolling='auto'></iframe> {/* Adjust height calculation based on NavBar and Footer */}
+      </div>
   )
 }
